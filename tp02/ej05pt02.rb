a845fd45393e8bf9d2ae907fc67a196a5d285e6b
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-09-20 10:40:30 -0300

    Hecho ejercicio 5 tp02.

--- tp02/ej05pt02.rb
+++ tp02/ej05pt02.rb
@@ -2,3 +2,12 @@
 # Time y que calcule la cantidad de minutos que faltan para ese momento. Si el parámetro de fecha
 # no es provisto, asumí que la consulta es para la medianoche de hoy.
 
+def cuanto_falta? (date = Time.new(Time.new.year, Time.new.month, Time.new.day, "+24:00"))
+	now = Time.now
+	date.to_i - now.to_i
+end
+
+
+puts cuanto_falta?
+
+puts cuanto_falta? (Time.new(2015, 10, 31, 23, 59, 59))
