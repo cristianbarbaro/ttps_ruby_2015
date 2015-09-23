6f59214db2796db0fb7cc69c35157ce4f23bf681
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-09-22 22:21:24 -0300

    Hecho ej 12 tp01. Sugerencia de Nati.

--- tp01/ej12tp01.rb
+++ tp01/ej12tp01.rb
@@ -2,5 +2,12 @@
 # string y un número n , y que realice una rotación de n lugares de las letras del string y
 # retorne el resultado.
 
+def rot (str, n)
+  str2 = str
+  n.times do
+    str2 = str2.tr('A-Za-z', 'B-ZAb-za')
+  end
+  str2
+end
 
-
+puts rot("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!", 27)
