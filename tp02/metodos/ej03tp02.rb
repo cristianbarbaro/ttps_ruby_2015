c9144b6ad17ada77854369bf93e0d2fcd0f2c82e
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-10-10 15:28:42 -0300

    Otro reordenamiento.

--- tp02/metodos/ej03tp02.rb
+++ tp02/metodos/ej03tp02.rb
@@ -1,6 +1,6 @@
 # Suponé que se te da el método que implementaste en el ejercicio anterior para que
 # lo uses a fin de ordenar un arreglo de números que te son provistos en forma de arreglo.
-# ¿Cómo podrías invocar el método? 
+# ¿Cómo podrías invocar el método?
 
 def ordenar (*elements)
 	elements.sort
--- tp02/metodos/ej04tp02.rb
+++ tp02/metodos/ej04tp02.rb
@@ -3,8 +3,7 @@
 # representación que se utilizó para contarla.
 
 def longitud (*str)
-	#Cuidado con poner el * o no funcionará correctamente el programa, arrojando un error (por esa boudez, estuve mucho tiempo colgado)
 	str.each { |s| puts "#{s.to_s} --> #{s.to_s.size}"}
 end
 
-longitud(9,Time.now,'Hola',{un: 'hash'},:ruby)
+longitud(9,Time.now,'Hola',{un: 'hash'},:ruby, :unBruno)
