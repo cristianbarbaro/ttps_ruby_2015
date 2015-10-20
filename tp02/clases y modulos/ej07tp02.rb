5b35084a217a85b8fc764b12c8bf11cbda02aa3b
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-10-20 18:17:06 -0300

    Agregado enunciado en ej07tp02.

--- tp02/clases y modulos/ej07tp02.rb
+++ tp02/clases y modulos/ej07tp02.rb
@@ -1,3 +1,12 @@
+# Extendé las clases TrueClass y FalseClass para que ambas respondan al método de instancia opposite, el cual en cada caso debe retornar el valor opuesto al que recibe la invocación al método. Por ejemplo:
+# 
+# false.opposite
+# # => true
+# true.opposite
+# # => false
+# true.opposite.opposite
+# # => true
+
 class TrueClass
   def opposite
     false
