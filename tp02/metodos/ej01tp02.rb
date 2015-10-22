26b7376f1820e0e4d64f38124fbe612e31b8bed2
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-10-22 09:23:06 -0300

    Hecha modificaciones para testear ej 1 de metodos.

--- tp02/metodos/ej01tp02.rb
+++ tp02/metodos/ej01tp02.rb
@@ -1,11 +1,13 @@
-# Implementá un método que reciba como parámetro un arreglo de números, los ordene y devuelva el 
+# Implementá un método que reciba como parámetro un arreglo de números, los ordene y devuelva el
 # resultado.
 #
 #
 def ordenar_arreglo (array)
-	array.sort
+	if array.is_a?(Array)
+		array.sort
+	else
+		raise TypeError
+	end
 end
 
-
 puts ordenar_arreglo [1, 4, 6, 2, 3, 0, 10]
-
